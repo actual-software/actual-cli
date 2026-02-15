a5aa70dc49ce64448586d5eafee67ab699e42b5e
poiley <benjpoile@gmail.com>
2026-02-14 22:19:37 -0800

    actual-cli-clf: inline write_single_file to eliminate phantom uncovered function

--- src/cli/commands/sync.rs
+++ src/cli/commands/sync.rs
@@ -1,10 +1,6 @@
 use crate::cli::args::SyncArgs;
-use crate::generation::writer;
 
 pub fn exec(_args: &SyncArgs) -> i32 {
-    // TODO: implement sync pipeline (actual-cli-9ur)
-    let _ = writer::write_files
-        as fn(&std::path::Path, &[crate::tailoring::types::FileOutput]) -> Vec<writer::WriteResult>;
     eprintln!("not implemented yet");
     0
 }
